8eea2f8ed7ff54f0dfdf2a6595a229eb12690eec
doublek <kian@Kians-MacBook-Pro-2.local>
2023-08-21 10:41:37 -0400

    finished front end

---
+++ app.js
@@ -0,0 +1,40 @@
+const form = document.getElementById("myForm");
+const result = document.getElementById("result");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    result.innerHTML = ""  
+
+    // What the User entered
+    const inputValue = form.querySelector("input").value;
+    // token from local session storage
+    // TODO: find a more secure way to cache the token during session
+    const token = localStorage.getItem('token')
+    const apiUrl = 'https://electrical-db-service.onrender.com/api/data';
+
+    fetch(apiUrl+`?param=${inputValue}`, { method: 'GET', headers: {
+        Authorization: `Bearer ${token}`
+    } })
+    .then(response => {
+        if(response.status !== 200) {
+            return response.json().then(data => {
+                throw new Error(data.message);
+            })
+        }
+        return response.json();
+    })
+    .then(data => {
+        for(let key in data[inputValue]) {
+            const content = document.createElement("p");
+            content.textContent = `${data[inputValue][key]}`
+            result.appendChild(content);
+        }
+    })
+    .catch(error => {
+        const errorMessage = document.createElement("p");
+        errorMessage.textContent = error.message;
+        errorMessage.style.color = "red";
+        result.appendChild(errorMessage);
+    });
+
+});
---
+++ login.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const result = document.getElementById("result");
+    const loginForm = document.getElementById('loginForm'); 
+  
+    loginForm.addEventListener('submit', async event => {
+      event.preventDefault();
+      result.innerHTML = "" 
+  
+      const username = loginForm.username.value;
+      const password = loginForm.password.value;
+  
+      try {
+        const response = await fetch('https://electrical-db-service.onrender.com/api/authenticate', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ username, password })
+        });
+  
+        if (response.ok) {
+          const data = await response.json();
+          const token = data.token;
+  
+          // Store the token in localStorage
+          localStorage.setItem('token', token);
+  
+          // Redirect to the protected page
+          window.location.href = 'app.html';
+        } else {
+          console.error('Login failed');
+          const errorMessage = document.createElement("p");
+          errorMessage.textContent = "Login Failed: Incorrect Username or Password";
+          errorMessage.style.color = "red";
+          result.appendChild(errorMessage);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    });
+  });
+  
