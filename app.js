9d47000e4c0023b5eed8d7dc41bd1c40ef7801c6
doublek <kian@Kians-MacBook-Pro-2.local>
2023-08-22 11:17:52 -0400

    securely logging out

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const form = document.getElementById("myForm");
 const result = document.getElementById("result");
 
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function (event) {
     event.preventDefault();
-    result.innerHTML = ""  
+    result.innerHTML = ""
 
     // What the User entered
     const inputValue = form.querySelector("input").value;
@@ -13,34 +13,38 @@ form.addEventListener("submit", function(event) {
     const apiUrl = 'https://electrical-db-service.onrender.com/api/data';
 
     fetch(apiUrl,
-    {   
-        method: 'POST', 
-        headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ inputValue })
-    })
-    .then(response => {
-        if(response.status !== 200) {
-            return response.json().then(data => {
-                throw new Error(data.message);
-            })
-        }
-        return response.json();
-    })
-    .then(data => {
-        for(let key in data[inputValue]) {
-            const content = document.createElement("p");
-            content.textContent = `${data[inputValue][key]}`
-            result.appendChild(content);
-        }
-    })
-    .catch(error => {
-        const errorMessage = document.createElement("p");
-        errorMessage.textContent = error.message;
-        errorMessage.style.color = "red";
-        result.appendChild(errorMessage);
-    });
+        {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ inputValue })
+        })
+        .then(response => {
+            if (response.status !== 200) {
+                return response.json().then(data => {
+                    throw new Error(data.message);
+                })
+            }
+            return response.json();
+        })
+        .then(data => {
+            for (let key in data[inputValue]) {
+                const content = document.createElement("p");
+                content.textContent = `${data[inputValue][key]}`
+                result.appendChild(content);
+            }
+        })
+        .catch(error => {
+            const errorMessage = document.createElement("p");
+            errorMessage.textContent = error.message;
+            errorMessage.style.color = "red";
+            result.appendChild(errorMessage);
+        });
 
 });
+
+window.addEventListener('beforeunload', function () {
+    localStorage.removeItem('token');
+})
